f45dfbe22f3e33485f429e9a962aae50ccc42f8e
lykuong1995 <lykuong1995@gmail.com>
2025-11-07 11:30:40 +0700

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config, { isServer }) => {
+    config.ignoreWarnings = [
+      ...(config.ignoreWarnings || []),
+      { module: /@supabase\/realtime-js/ },
+    ];
+    return config;
+  },
 };
 
 module.exports = nextConfig;
